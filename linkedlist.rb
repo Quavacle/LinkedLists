61f80e873e6696860b7a158a53d604bfdf491321
Bronson Valiquet Myers <owd.bob@gmail.com>
2019-09-17 20:53:45 +0000

    Reversed pointers in a linked list without a stack

--- linkedlist.rb
+++ linkedlist.rb
@@ -25,52 +25,66 @@ class Stack
   end
 end
 
-def reverse_list(list)
-  stack = Stack.new
-  while list != nil
-    stack.push(list.value)
-    list = list.next_node
+def reverse_pointer(node, previous = nil)
+  #Check existence of next node
+  if node.next_node != nil
+    #Call the next node, passing current node's location
+    reverse_pointer(node.next_node, node)
+    #Redefine the node's next node call to the previous value
+    node.next_node = previous  
+  else
+    #When next node is nil, simply set the current nodes next to prior node
+    node.next_node = previous
+    return
   end
+
+end
+
+def reverse_list(list)
+  #Initialize stack
+  stack = Stack.new
+  #Check existence of node
+  while list != nil
+    #Push value of current node into stack
+    stack.push(list.value)
+    #Move to next node
+    list = list.next_node   
+  end
+  #Commit changes to stack
   stack.data
 end
 
 def print_values(list_node)
-  if list_node
+  #Check existence of node
+  if list_node != nil
+    #Print value of current node
     print "#{list_node.value} -->"
+    #Call function again on next node
     print_values(list_node.next_node)
   else
     print "nil"
     return
   end
+  #Aesthetic spacer only
   puts ""
 end
 
-node1 = LinkedListNode.new(37)
-node2 = LinkedListNode.new(99, node1)
-node3 = LinkedListNode.new(12, node2)
+node1 = LinkedListNode.new(1)
+node2 = LinkedListNode.new(66, node1)
+node3 = LinkedListNode.new(3, node2)
+node4 = LinkedListNode.new(404, node3)
 
 puts "Original stack:"
 puts "---------------"
-print_values(node3)
+print_values(node4)
+
+puts "Reversed stack:"
 puts "---------------"
-reverse = reverse_list(node3)
-puts "      Reversed:"
-puts "---------------"
+reverse = reverse_list(node4)
 print_values(reverse)
+
+puts ""
+puts "Reversed pointers (No stack):"
 puts "---------------"
-puts " "
-# puts "- OR -"
-# puts "Ruby and its wondrous memory allocation!"
-# puts "-----------"
-# puts "Pushing 1, 2, 3, 4, 5"
-# arr = []
-# arr.push(1, 2, 3, 4, 5)
-# puts "-----------"
-# puts "Listing in order"
-# puts arr
-# puts "-----------"
-# puts "Last item popped: #{arr.pop}"
-# arr.push(5)
-# puts "-----------"
-# puts "Reversed"
-# puts arr.reverse
+reverse_pointer(node4)
+print_values(node1)
